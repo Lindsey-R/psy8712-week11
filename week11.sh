cec4c7bd1ff0e972b310b34f313698fa18d8d62e
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-04-07 19:39:11 -0500

    Updated week11.sh

--- week11.sh
+++ week11.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=30
 #SBATCH --mem=16gb
-#SBATCH -t 12:00:00
+#SBATCH -t 01:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ren00172@umn.edu
 #SBATCH -p amdsmall
